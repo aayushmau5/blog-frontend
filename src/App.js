c7a74341df5f3836488487d2e8a729630b8c28a8
Aayush Kumar Sahu <aayushmau5@gmail.com>
2020-12-23 16:31:01 +0530

    Add tailwind css configs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Blogs</h1>
-      </header>
+      <div class="p-6 max-w-sm mx-auto bg-green-100 rounded-xl shadow-md flex items-center space-x-4">
+        <div>
+          <div class="text-xl font-medium text-black">ChitChat</div>
+          <p class="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
     </div>
   );
 }
