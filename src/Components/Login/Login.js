06e0a5e50373f7da7e43bb6b01237a07b76aeb2a
Aayush Kumar Sahu <aayushmau5@gmail.com>
2020-12-27 20:34:58 +0530

    Add Login and Signup Functionality

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,14 +1,44 @@
 import { useForm } from "react-hook-form";
 import "./Login.css";
+import { useState } from "react";
+import axios from 'axios';
 
 function Login() {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [loginError, setLoginError] = useState("");
+  const [usernameError, setUsernameError] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+
+  const onSubmit = (data) => {
+    setSubmitting(true);
+    setUsernameError("");
+    setLoginError("");
+    axios
+      .post("/user/login", data)
+      .then((result) => {
+        if (result.status === 200) {
+          setSubmitting(false);
+          console.log(result);
+        }
+      })
+      .catch((err) => {
+        setSubmitting(false);
+        if (err.response.status === 404) {
+          setLoginError(err.response.data.error);
+        } else if (err.response.status === 422) {
+          setUsernameError(err.response.data.errors[0].username);
+        }
+      });
+  };
 
   return (
     <>
       <div className="form-container">
         <form onSubmit={handleSubmit(onSubmit)} className="form">
+          {loginError !== "" && (
+            <span className="form-login-error">{loginError}</span>
+          )}
+          {submitting && <div className="form-login-submit">Submitting....</div>}
           <div className="form-control">
             <label htmlFor="username">Username:</label>
             <input
@@ -19,19 +49,29 @@ function Login() {
             {errors.username && errors.username.type === "required" && (
               <span className="form-error">Username is required</span>
             )}
+            {usernameError !== "" && (
+              <span className="form-error">{usernameError}</span>
+            )}
           </div>
           <div className="form-control">
             <label htmlFor="password">Password:</label>
             <input
               type="password"
-              ref={register({ required: true })}
+              ref={register({ required: true, minLength: 6 })}
               name="password"
             />
             {errors.password && (
               <span className="form-error">Password is required</span>
             )}
+            {errors.password && errors.password.type === "minLength" && (
+              <span className="form-error">
+                Password must be atleast 6 characters long
+              </span>
+            )}
           </div>
-          <button className="submit-btn" type="submit">Login</button>
+          <button className="submit-btn" type="submit">
+            Login
+          </button>
         </form>
       </div>
     </>
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -1,12 +1,43 @@
+import axios from "axios";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
+import styles from "./Signup.module.css";
 
 function Signup() {
   const { register, handleSubmit, errors } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const [usernameError, setUsernameError] = useState("");
+  const [signedIn, setSignedIn] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
+
+  const onSubmit = (data) => {
+    setSubmitting(true);
+    setUsernameError("");
+    axios
+      .post("/user/signup", data)
+      .then((result) => {
+        if (result.status === 200) {
+          setSubmitting(false);
+          setSignedIn(true);
+        }
+      })
+      .catch((err) => {
+        setSignedIn(false);
+        setSubmitting(false);
+        if (err.response.status === 409) {
+          setUsernameError("Username already present");
+        } else if (err.response.status === 422) {
+          setUsernameError(err.response.data.errors[0].username);
+        }
+      });
+  };
 
   return (
     <>
       <div className="form-container">
+        {signedIn && (
+          <div className={styles.signupIndicator}>Signed Up successfully</div>
+        )}
+        {submitting && <div>Submitting....</div>}
         <form onSubmit={handleSubmit(onSubmit)} className="form">
           <div className="form-control">
             <label htmlFor="username">Username:</label>
@@ -18,16 +49,24 @@ function Signup() {
             {errors.username && errors.username.type === "required" && (
               <span className="form-error">Username is required</span>
             )}
+            {usernameError !== "" && (
+              <span className="form-error">{usernameError}</span>
+            )}
           </div>
           <div className="form-control">
             <label htmlFor="password">Password:</label>
             <input
               type="password"
-              ref={register({ required: true })}
+              ref={register({ required: true, minLength: 6 })}
               name="password"
             />
-            {errors.password && (
+            {errors.password && errors.password.type === "required" && (
               <span className="form-error">Password is required</span>
+            )}
+            {errors.password && errors.password.type === "minLength" && (
+              <span className="form-error">
+                Password must be atleast 6 characters long
+              </span>
             )}
           </div>
           <button className="submit-btn" type="submit">
